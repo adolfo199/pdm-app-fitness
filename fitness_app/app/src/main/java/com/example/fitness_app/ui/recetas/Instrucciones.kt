f47b0ea38d2ff5c7b8aa3f1904633550c5273222
Kevin Segovia <72715105+KevinDev99@users.noreply.github.com>
2022-04-08 11:21:43 -0600

    crear funcion ver recetas

--- fitness_app/app/src/main/java/com/example/fitness_app/ui/recetas/Instrucciones.kt
+++ fitness_app/app/src/main/java/com/example/fitness_app/ui/recetas/Instrucciones.kt
@@ -3,11 +3,72 @@ package com.example.fitness_app.ui.recetas
 class Intrucciones {
     companion object{
         val instruccionesEnsalada = listOf<String>(
-            "Lava muy bien los vegetales, y pica en tiras tanto la zanahoria como la cebolla, " +
-                    "el pimenton, la berenjena","Vierte  en una sarten un poco de aceite (previamente" +
-                    " caliente), deja que los vegetales comiencen a dorarse y a marchitarse.",
-            "Agrega una pizaca de agua pues no queremos un sofrito como tal, despues hecha la salsa " +
-                    "para pasta  y piemienta"
+            "LICÚA todos los ingredientes y salpimienta.",
+            "COLOCA los huevos en un cazo y vierte agua fría hasta cubrirlos. Cocina a fuego" +
+                    " medio y cuando suelte el hervor, cuenta 12 minutos.",
+            "RETIRA los huevos y colócalos en agua fría. Pela y corta en cuatro.",
+            "MEZCLA las hojas de espinaca, la lechuga, el jitomate y los champiñones.",
+            "AGREGA el huevo cocido y el aguacate. Mezcla con cuidado.",
+            "SIRVE la ensalada con un poco de aderezo y espolvorea el tocino."
+        )
+        val instruccionesYogurtConFrutas = listOf<String>(
+            "Bueno la preparación es realmente sencilla, coge las frutas y quitales la piel," +
+                    " corta en cubitos o rodajas como tu las prefieras.",
+            "A continuación en un tazo vierte el yogur de tu elección y añade las frutas," +
+                    " para terminar de complementar nuestro rico y nutritivo desayuno saludable," +
+                    " añade un la miel y ralladura de limón.",
+            "Listo asi de simple, ya esta terminado y podemos comenzar a degustar nuestro desayuno."
+        )
+        val instruccionesPancakesDeManzana = listOf<String>(
+            "Comienza añadiendo la harina en un recipiente junto al polvo para hornear, añade" +
+                    " también la canela, sal e integra bien todo.",
+            "Luego añade la vainilla, manzana y ve vertiendo la leche poco a poco, al mismo tiempo" +
+                    " mezcla bien todo, deberá quedarte una mezcla homogénea, si notas que esta muy" +
+                    " espeso añade mas leche o agua, de lo contrario si notas que quedo" +
+                    " muy liquido añade harina.",
+            "Calienta una sarten con un chorrito de aceite de oliva, ve añadiendo poco a poco" +
+                    " la mezcla, y cocina los pancakes a fuego bajo.",
+            "Una vez tengas todos listos, sirve en un plato adornando con las rodajas de" +
+                    " banano y trozos de manzana y miel por encima, te aseguro que no quedaras" +
+                    " indiferente con el sabor."
+        )
+        val instruccionesPolloAlHornoConPatatas = listOf<String>(
+            "Para esta receta de pollo al horno con patatas y cebolla comienza pelando las" +
+                    " patatas. Luego cortalas en rodajas algo delgadas y algo gruesas, vamos" +
+                    " en termino medio es decir algo como 1 cm o quizá algunos milímetros de" +
+                    " mas. Luego colócalos como base en una bandeja de horno.",
+            "Haz el mismo procedimiento con las cebollas y al igual que las patatas ponlas" +
+                    " como base sobre las patatas, sal pimienta a gusto.",
+            "Continuando añade el pollo encima de tu base de patatas y cebolla, añade un vaso" +
+                    " ojo un vaso de agua y un chorrito de aceite de oliva, esto nos otorgara" +
+                    " un jugo irresistible en nuestro pollo, ademas procurando que el fondo" +
+                    " de la bandeja este siempre húmeda para impedir que las patatas se" +
+                    " quemen. Añade una pizca de sal y tomillo.",
+            "Introduce el pollo dentro el horno a 190°C calor arriba y abajo, y dejamos hornea" +
+                    "r un periodo de 30 minutos.",
+            "Cuando el pollo este dorado, sácala del horno intenta darle la vuelta" +
+                    " y para adentro otra vez, deja cocinar otros 30 minutos y luego estará sácalo.",
+            "Ahora si nuestro pollo quedo jugoso, tierno y muy delicioso. Sirve acompañado" +
+                    " de la guarnición de patatas y cebolla que con el jugo que soltó" +
+                    " el pollo están para chuparse los dedos, buen provecho."
+        )
+        val intruccionesLomitoDeResRelleno = listOf<String>(
+            "Comenzamos lavando y cortando los champiñones en trocitos no tan pequeños," +
+                    " así mismo hacemos con la cebolla verde y el tocino. En una sarten doramos" +
+                    " los tocinos durante 2 minutos, hasta que expulse su propia grasa, en ese" +
+                    " momento agregamos champiñones y la cebolla verde, sal pimentamos y reposar.",
+            "Cortamos nuestro lomito por la mitad ojo no lo partimos, tan solo le hacemos un" +
+                    " corte como si fuese un pan, en la parte del centro introduciremos" +
+                    " nuestro sofrito y el queso rallado.",
+            "IMPORTANTE: Ve Pre-calentando el horno a 180°",
+            "Cerramos el lomito con ayuda de de unos mondadientes. En un sarten añadimos" +
+                    " unas gotas de aceite de oliva y freímos nuestro lomo, este paso solo" +
+                    " se trata de freír la parte de afuera.",
+            "Pela las zanahorias en tiras o en rodajas, como tu prefieras, y las vainitas" +
+                    " van enteras. Ahora lo llevamos todo al horno durante unos 40 minutos," +
+                    " dependerá de que tan cocido quieres que este el lomito.",
+            "Listo!! nuestro lomito relleno esta terminado. Sirve acompañado de" +
+                    " unas papas o una guarnición de verduras."
         )
     }
 }
